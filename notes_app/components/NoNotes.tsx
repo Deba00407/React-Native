6ef148036dc9660b7a7ff413ce3eb0ef49fcfb45
Deba00407 <cdebanjan456@gmail.com>
2026-05-10 21:13:39 +0530

    Finalized homescreen with no notes

--- notes_app/components/NoNotes.tsx
+++ notes_app/components/NoNotes.tsx
@@ -1,17 +1,51 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import SafeWrapper from "./SafeWrapper";
 
 const NoNotesScreen = () => {
-    return (
-        <View>
-            <Text>Welcome to our App!</Text>
-        </View>
-    )
-}
+  return (
+    <SafeWrapper>
+      <View style={styles.container}>
+        <Image
+          source={require("@/assets/my-images/illustrations/notes-illustration.png")}
+          style={styles.illustration}
+        />
+        <Text style={styles.head_text}>Your digital paper is blank!</Text>
+        <Text style={styles.sub_head_text}>
+          Add a new note to start capturing your thoughts and ideas
+        </Text>
+        <Text>Let the creativity unleash!!</Text>
+      </View>
+    </SafeWrapper>
+  );
+};
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-})
+  illustration: {
+    width: 220,
+    height: 220,
+    resizeMode: "contain",
+  },
+
+  head_text: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+
+  sub_head_text: {
+    fontSize: 16,
+    fontWeight: "500",
+    maxWidth: 280,
+    textAlign: "center",
+    lineHeight: 24,
+    marginTop: 8,
+  },
+});
 
 export default NoNotesScreen;
-
